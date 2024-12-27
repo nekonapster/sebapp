b41a639536c927b2cfa95a9626d0a802de494430
nekonapster <mdmgrafica@gmail.com>
2024-12-27 12:13:11 +0100

    implementacion del chart terminada, corrijo el tipo que se sube a la base de datos desde el modelo 'saldo' y 'bases'

--- app/Livewire/ChartComponent.php
+++ app/Livewire/ChartComponent.php
@@ -2,51 +2,85 @@
 
 namespace App\Livewire;
 
+use App\Models\Base;
 use App\Models\Saldo;
 use Carbon\Carbon;
 use Livewire\Component;
 
 class ChartComponent extends Component
 {
-    public $saldosPorMes = [];
     public $fechaActual;
+    public $ingresosDelMes = [];
+    public $egresosDelMes = [];
+    public $inicioMes;
+    public $finMes;
 
     public function mount()
     {
-        $this->ingresosPorMes();
+        // $this->inicioMes = Carbon::now()->startOfMonth();
+        // $this->finMes  = Carbon::now()->endOfMonth();
         $this->fechaActual = Carbon::now();
+
+        $this->ingresosPorMes();
+        $this->egresosPorMes();
     }
     
-
+    // ingresos del mes = sum de 4xxx que esten en pagadas (true)
     public function ingresosPorMes()
     {
-        $this->saldosPorMes = [];
+        $this->ingresosDelMes = [];
     
         for ($i = 1; $i <= 12; $i++) {
             $inicioMes = Carbon::create(null, $i, 1)->startOfMonth();
             $finMes = Carbon::create(null, $i, 1)->endOfMonth();
             $diasDelMes = $inicioMes->daysInMonth;
     
-            $saldosDiarios = array_fill(1, $diasDelMes, 0);
+            $ingresosDiarios = array_fill(1, $diasDelMes, 0);
     
             for ($dia = 1; $dia <= $diasDelMes; $dia++) {
-                $fecha = Carbon::create(null, $i, $dia);
-                $saldo = Saldo::whereDate('created_at', $fecha)->sum('calcularTotal');
-                $saldosDiarios[$dia] = $saldo;
+                $saldo = Base::where('cc', 'like', '4%')
+                    ->where('estado', true)
+                    ->whereBetween('created_at', [$inicioMes, $finMes])
+                    ->sum('importe');
+                $ingresosDiarios[$dia] = $saldo;
             }
     
             $nombreMes = Carbon::create(null, $i, 1)->translatedFormat('F');
-            $this->saldosPorMes[$nombreMes] = $saldosDiarios;
+            $this->ingresosDelMes[$nombreMes] = $ingresosDiarios;
         }
     }
     
+    // egresos pagados del mes = sum de 5xxx que esten en pagadas (true)
+    public function egresosPorMes()
+    {
+        $this->egresosDelMes = [];
     
+        for ($i = 1; $i <= 12; $i++) {
+            $inicioMes = Carbon::create(null, $i, 1)->startOfMonth();
+            $finMes = Carbon::create(null, $i, 1)->endOfMonth();
+            $diasDelMes = $inicioMes->daysInMonth;
+    
+            $egresosDiarios = array_fill(1, $diasDelMes, 0);
+    
+            for ($dia = 1; $dia <= $diasDelMes; $dia++) {
+                $saldo = Base::where('cc', 'like', '5%')
+                    ->where('estado', true)
+                    ->whereBetween('created_at', [$inicioMes, $finMes])
+                    ->sum('importe');
+                $egresosDiarios[$dia] = $saldo;
+            }
+    
+            $nombreMes = Carbon::create(null, $i, 1)->translatedFormat('F');
+            $this->egresosDelMes[$nombreMes] = $egresosDiarios;
+        }
+    }
     
 
     public function render()
     {
         return view('livewire.chart-component', [
-            'saldosPorMes' => $this->saldosPorMes,
+            'ingresosDelMes' => $this->ingresosDelMes,
+            'egresosDelMes' => $this->egresosDelMes,
         ]);
     }
 }
--- app/Models/Saldo.php
+++ app/Models/Saldo.php
@@ -31,5 +31,11 @@ class Saldo extends Model
     //     'calcularTotal' => 'integer',
     //     'fechaSaldos' => 'datetime',
     // ];
+
+       // Setter para convertir a número antes de guardar
+       public function setCalcularTotalAttribute($value)
+       {
+           $this->attributes['calcularTotal'] = is_numeric($value) ? (float) $value : null; // Convierte a float o guarda null si no es válido
+       }
 }
 
