f51324076ddf6095a03b266fe67093a18ac95963
NekoNapster <mdmgrafica@gmail.com>
2025-01-13 18:01:01 +0100

    quito los source de jquery del @vite

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js','node_modules/jquery/dist/jquery.min.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireScripts
     </head>
     <body class="font-sans text-gray-900 antialiased">
--- resources/views/layouts/invitado.blade.php
+++ resources/views/layouts/invitado.blade.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js','node_modules/jquery/dist/jquery.min.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
     </html>
