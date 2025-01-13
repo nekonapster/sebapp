428b0415c93d9ea83c569cc0274b0b7e8a82101b
NekoNapster <mdmgrafica@gmail.com>
2025-01-13 17:34:15 +0100

    añado la ruta generada en el manifest.json a la entrada app.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js','public/build/assets/jquery.min'])
+        @vite(['resources/css/app.css', 'resources/js/app.js','assets/jquery.min-iLCsbXYO.js'])
         @livewireStyles
 
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js','assets/jquery.min-iLCsbXYO.js'])
         @livewireScripts
     </head>
     <body class="font-sans text-gray-900 antialiased">
